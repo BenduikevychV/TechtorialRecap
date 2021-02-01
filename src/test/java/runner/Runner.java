648e89a78ff50fa371cdd7367eaff911614f23bc
BenduikevychV <volodymyrbend@gmail.com>
2021-01-31 20:46:22 -0600

    added plugins

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/feature/APIGetEmployeeCall.feature",
+        plugin = {"pretty","html:target/cucumber-html-report","junit:target/cucumber.xml",
+                "rerun:target/rerun.txt","json:target/cucumber.json"},
         glue = "steps",
         tags = "@api",
         dryRun = false
